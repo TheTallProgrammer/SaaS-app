676ea06e316fa4d1c24aa7671652c2af5e9eda63
Logan Falkenberg <loganf0101@gmail.com>
2025-06-17 21:56:49 -0500

    making commit

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,8 +1,13 @@
+import CompanionForm from "@/components/CompanionForm"
+
 const NewCompanion = () => {
   return (
-    <div>
-      New
-    </div>
+    <main>
+      <article>
+        <h1>Companion Builder</h1>
+        <CompanionForm/>
+      </article>
+    </main>
   )
 }
 
